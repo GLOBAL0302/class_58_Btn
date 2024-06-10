a5bdce5a2b5dbc1a1058205aa2a158c3db7694be
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2024-06-11 03:04:21 +0400

    added alert components

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@ import './App.css';
 import Modal from './components/Modal/Modal';
 import { useState } from 'react';
 import { IButton } from '../types';
+import Alert from './components/Alert/Alert';
 
 const App = () =>{
   const [showModal, setShowModal] = useState(false)
+  const [showAlert, setShowAlert] = useState(false)
   const buttons:IButton[] = [
     {type: 'primary', label: 'Continue', onClick: () => console.log('clicked continue')},
     {type: 'danger', label: 'Close', onClick: () => setShowModal(false)}
   ]
 
+
+  const closeAlert=()=>{
+    setShowAlert(!showAlert)
+  }
+
   return(
-    <>
-      <div className="myModal">
+    <div className="d-flex align-items-center justify-content-center pt-5">
+      <div className="myModal col-6">
         <Modal
           show={showModal}
           onClose={()=>setShowModal(!showModal)}
@@ -21,11 +28,17 @@ const App = () =>{
         >
           <p>This is modal content</p>
         </Modal>
-        <button onClick={()=>setShowModal(!showModal)}>
+        <button onClick={()=>setShowModal(true)}>
           Modal
         </button>
       </div>
-    </>
+      <div className="myAlert col-6">
+        {showAlert?(<Alert
+          type={"warning"}
+        >This is a warning alert</Alert>):""}
+        <button onClick={()=>setShowAlert(true)}>Alert</button>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,21 @@
+import { PropsWithChildren } from 'react';
+
+interface Props extends PropsWithChildren{
+  type:string
+  onDismiss:()=>void
+}
+
+const Alert:React.FC<Props> = ({type, onDismiss, children}) => {
+  return (
+    <div className={`alert alert-${type}`} role="alert">
+      {children}
+      {onDismiss?<button
+        onClick={onDismiss}
+        className='ms-2' style={{border:"none", borderRadius:"50%"}}>
+        X
+      </button>:""}
+    </div>
+  );
+};
+
+export default Alert;
